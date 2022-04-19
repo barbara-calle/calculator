1aa377ef1ec7e62d3a0cfb51d29fd12d0013fca4
barbara-calle <barbara.calle@globant.com>
2022-04-19 18:25:30 -0500

    Added a method for sum and subtract

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    static void sum(int a, int b){
+        System.out.println(a + b);
+    }
+
+    static void subtract(int a, int b){
+        System.out.println(a - b);
+    }
+    public static void main (String[] args){
+        sum(4, 34);
+        subtract(38, 40);
+    }
+}
